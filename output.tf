e9ea1bd1ad91dea4518545b79f5a5d88e156fafb
matheuspassini <matheusbedini@yahoo.com.br>
2021-12-14 15:54:05 -0300

    second

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "creation_timestamp" {
+  value = google_compute_subnetwork.subnet.creation_timestamp
+}
+
+output "gateway_address" {
+  value = google_compute_subnetwork.subnet.gateway_address
+}
+
+output "self_link" {
+  value = google_compute_subnetwork.subnet.self_link
+}
